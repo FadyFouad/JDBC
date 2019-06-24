c18555fc750227f9b3f259e07ff1c3cd5888b23b
fadyfouad <fady.fouad.a@gmail.com>
2019-06-24 20:00:58 +0200

    Create table

--- src/com/etaTech/Main.java
+++ src/com/etaTech/Main.java
@@ -3,6 +3,7 @@ package com.etaTech;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /****************************************************
  *** Created by Fady Fouad on 6/23/2019 at 15:11.***
@@ -11,9 +12,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("----------------------DATABASES-------------------------");
         try {
-//            Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\USER\\music_JDBC.db");
-            Connection connection = DriverManager.getConnection("jdbc:sqlite:music_JDBC.db");
-            
+//            Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\USER\\Fady_JDBC.db");
+            Connection connection = DriverManager.getConnection("jdbc:sqlite:Fady_JDBC.db");
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE Fady (name TEXT , phone INTEGER,email TEXT )");
 
         }catch (SQLException e){
             System.out.println(e.getMessage());
