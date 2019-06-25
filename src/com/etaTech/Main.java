d72609e6cea77d8097fb30c99b8c50f9a233b88d
fadyfouad <fady.fouad.a@gmail.com>
2019-06-25 09:54:53 +0200

    INSERT , UPDATE , DELETE , SELECT

--- src/com/etaTech/Main.java
+++ src/com/etaTech/Main.java
@@ -1,9 +1,7 @@
 package com.etaTech;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
 
 /****************************************************
  *** Created by Fady Fouad on 6/23/2019 at 15:11.***
@@ -14,10 +12,22 @@ public class Main {
         try (Connection connection = DriverManager.getConnection("jdbc:sqlite:Fady_JDBC.db");
              Statement statement = connection.createStatement()) {
 //            Connection connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\USER\\Fady_JDBC.db");
-            statement.execute("CREATE TABLE IF NOT EXISTS Fady " +
-                                  "(name TEXT , phone INTEGER,email TEXT )");
-//            statement.close();
-//            connection.close();//try with res auto close.
+//            connection.setAutoCommit(false);
+//            statement.execute("CREATE TABLE IF NOT EXISTS Fady " +
+//                                  "(name TEXT , phone INTEGER,email TEXT )");
+//            statement.execute("INSERT INTO Fady " +
+//                                  "VALUES ('Fadi',201123815517,'fady.fouad.a@gmail.com')");
+//            statement.execute("INSERT INTO Fady " +
+//                                  "VALUES ('Fady99',201123815517,'fady.fouad.a@gmail.com')");
+//            statement.execute("UPDATE Fady SET email = 'fadyfouad993@gmail' WHERE  name = 'Fadi'" );
+//            statement.execute("DELETE FROM Fady WHERE name = 'Fady99'");
+
+            statement.execute("SELECT * FROM Fady ");
+            ResultSet resultSet = statement.getResultSet();
+            while (resultSet.next()){
+                System.out.println(resultSet.getString("name")+" : "+resultSet.getString("phone")+" : "+resultSet.getString("email"));
+            }
+
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
